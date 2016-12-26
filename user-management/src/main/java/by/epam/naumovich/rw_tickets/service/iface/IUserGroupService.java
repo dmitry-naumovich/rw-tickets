fc315c7b9feb3b17d42853609c4f902374d2165e
Dzmitry Naumovich <dzmitry_naumovich@epam.com>
2016-12-26 17:06:03 +0300

    service interfaces created, started working on service layer

---
+++ user-management/src/main/java/by/epam/naumovich/rw_tickets/service/iface/IUserGroupService.java
@@ -0,0 +1,13 @@
+package by.epam.naumovich.rw_tickets.service.iface;
+
+/**
+* Defines methods that receive parameters from Command implementations, verify them, construct necessary entities if needed 
+* and then pass them to the DAO layer, possibly getting some objects or primitive values back and passing them further back to the commands.
+* Works with the UserGroup entity mostly.
+* 
+* @author Dmitry Naumovich
+* @version 1.0
+*/
+public interface IUserGroupService {
+
+}
---
+++ user-management/src/main/java/by/epam/naumovich/rw_tickets/service/iface/IUserService.java
@@ -0,0 +1,33 @@
+package by.epam.naumovich.rw_tickets.service.iface;
+
+import java.util.List;
+
+import by.epam.naumovich.rw_tickets.entity.User;
+import by.epam.naumovich.rw_tickets.service.util.USER_SORT_TYPE;
+
+/**
+ * Defines methods that receive parameters from Command implementations, verify them, construct necessary entities if needed 
+ * and then pass them to the DAO layer, possibly getting some objects or primitive values back and passing them further back to the commands.
+ * Works with the User entity mostly.
+ * 
+ * @author Dmitry Naumovich
+ * @version 1.0
+ */
+public interface IUserService {
+
+	int addUser(User user);
+	void updateUser(int id, User updUser);
+	void deleteUser(int id);
+	User getUserById(int id);
+	User getUserByLogin(String login);
+	List<User> getAllUsers();
+	List<User> getAllGroupUsers(int groupID);
+	String getPasswordByLogin(String login);
+	int getIDByLogin(String login);
+	String getPasswordByEmail(String email);
+	List<User> getUsersByName(String name);
+	List<User> getUsersBySurname(String surname);	
+	List<User> getUsersByCountry(String country);	
+	List<User> getUsersByCity(String city);	
+	List<User> getAllUsersSorted(USER_SORT_TYPE type);	
+}
